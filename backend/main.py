24919dc097d432af303d862b70010ddd46c1f762
Adam Kariv <adam.kariv@gmail.com>
2021-05-21 15:16:00 +0300

    restructure

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-import json
-from .new_selfie import new_selfie_handler
-from .get_game import get_game_handler
+from .lib.new_selfie import new_selfie_handler
+from .lib.get_game import get_game_handler
 
