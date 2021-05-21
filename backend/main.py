d02d6bdf7dcaea648d27b2842cf1ea15e02f6b5e
Adam Kariv <adam.kariv@gmail.com>
2021-05-21 15:20:21 +0300

    fix import?

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
-from .lib.new_selfie import new_selfie_handler
-from .lib.get_game import get_game_handler
+from lib.new_selfie import new_selfie_handler
+from lib.get_game import get_game_handler
 
