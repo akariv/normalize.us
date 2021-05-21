5931bc3b1e91c28723848a438787eebb1df9a283
Adam Kariv <adam.kariv@gmail.com>
2021-05-21 21:53:47 +0300

    Fix prod environment

--- projects/normalize/src/environments/environment.prod.ts
+++ projects/normalize/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   endpoints: {
-    new: 'http://localhost:8080/'
+    new: 'https://europe-west3-normalize-us.cloudfunctions.net/upload-selfie',
+    getGame: 'https://europe-west3-normalize-us.cloudfunctions.net/get-game',
+    getImage: 'https://europe-west3-normalize-us.cloudfunctions.net/get-image',
+    gameResults: 'https://europe-west3-normalize-us.cloudfunctions.net/game-results',
   }
 };
