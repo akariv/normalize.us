f60559fba82d1f9189f9c5913b4bd41e3abf6184
Adam Kariv <adam.kariv@gmail.com>
2021-06-05 09:09:43 +0300

    Add tsne calculation

--- backend/main.py
+++ backend/main.py
@@ -2,4 +2,4 @@ from lib.new_selfie import new_selfie_handler
 from lib.get_game import get_game_handler
 from lib.game_results import game_results_handler
 from lib.get_image import get_image_handler
-
+from lib.calc_tsne import calc_tsne_handler
