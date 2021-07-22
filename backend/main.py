e9bf891b642ddc879061a7365b4b8d28ebd098e2
Adam Kariv <adam.kariv@gmail.com>
2021-07-22 18:42:58 +0300

    Add get-latest endpoint 2

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ from lib.get_game import get_game_handler
 from lib.game_results import game_results_handler
 from lib.get_image import get_image_handler
 from lib.calc_tsne import calc_tsne_handler
-from lib.latest import calc_tsne_handler
+from lib.latest import get_latest
