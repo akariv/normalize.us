95a3ae2bf76b3aef2887b75d80844d9dcb5c8f37
Adam Kariv <adam.kariv@gmail.com>
2021-07-22 12:06:26 +0300

    Add latest endpoint

---
+++ backend/lib/latest.py
@@ -0,0 +1,38 @@
+import json
+from sqlalchemy.sql import text
+from flask import Request, Response
+
+from .db import connection
+from .net import HEADERS
+
+fetch_latest = text('''
+    SELECT id, image, votes, tournaments, descriptor, landmarks
+    FROM faces
+    ORDER BY id desc
+    LIMIT 5
+''')
+
+
+def get_game_handler(request: Request):
+    if request.method == 'OPTIONS':
+        return Response('', headers=HEADERS)
+    if request.method == 'GET':
+        rows = connection.execute(fetch_latest)
+        result = []
+        for row in rows:
+            row = dict(row)
+            result.append(row)
+        response = dict(
+            success=True, records=result
+        )
+        response = Response(
+            json.dumps(response),
+            headers=HEADERS
+        )
+        response.cache_control.max_age = 10
+        return response
+
+    return Response(
+        json.dumps(dict(success=False)),
+        headers=HEADERS
+    )
