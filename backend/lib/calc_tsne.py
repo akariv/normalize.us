2bcd063e01b4041d4ebe7e749ff64582546306fc
Adam Kariv <adam.kariv@gmail.com>
2021-08-04 09:31:14 +0300

    Fix photo location for tsne

--- backend/lib/calc_tsne.py
+++ backend/lib/calc_tsne.py
@@ -21,7 +21,7 @@ image_fetches = 0
 
 def load_image(id, out_res_x, out_res_y, img_location, img_size):
     global image_fetches
-    img_url = f'https://normalizing-us-files.fra1.cdn.digitaloceanspaces.com/{id}_full.png'
+    img_url = f'https://normalizing-us-files.fra1.cdn.digitaloceanspaces.com/photos/{id}_full.png'
     img_data = requests.get(img_url).content
     img = Image.open(BytesIO(img_data))
     img = img.crop((*img_location, img_location[0] + img_size[0], img_location[1] + img_size[1]))
