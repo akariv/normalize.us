5b8551ac4596b6c3e37fb0910454237d0726be87
Adam Kariv <adam.kariv@gmail.com>
2021-08-25 19:39:49 +0300

    +

--- backend/lib/db.py
+++ backend/lib/db.py
@@ -4,3 +4,5 @@ from sqlalchemy import create_engine
 if 'DATABASE_URL' in os.environ:
     engine = create_engine(os.environ['DATABASE_URL'])
     connection = engine.connect()
+else:
+    engine = connection = None
