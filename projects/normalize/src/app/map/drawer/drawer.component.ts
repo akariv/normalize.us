56eac6691d9cab10a8ed9e1ee4d5f4391e778922
Adam Kariv <adam.kariv@gmail.com>
2021-09-07 21:30:16 +0200

    Scroll drawer up when closing

--- projects/normalize/src/app/map/drawer/drawer.component.ts
+++ projects/normalize/src/app/map/drawer/drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
 import { LayoutService } from '../../layout.service';
 
 @Component({
@@ -13,15 +13,24 @@ import { LayoutService } from '../../layout.service';
     '[class.mobile]': 'layout.mobile',
   }
 })
-export class DrawerComponent implements OnInit {
+export class DrawerComponent implements OnInit, OnChanges {
 
   @Input() open = false;
   @Input() hidden = false;
   @Output() changed = new EventEmitter<boolean>();
 
+  @ViewChild('content') content: ElementRef;
+
   constructor(public layout: LayoutService) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    if (!this.open && this.content.nativeElement) {
+      const el: HTMLElement = this.content.nativeElement;
+      el.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   }
 
   state(value) {
