b05dfe19d3228089bf49d5052552034faedca923
Adam Kariv <adam.kariv@gmail.com>
2021-09-28 21:21:48 +0300

    Latest endpoint fix

--- backend/lib/latest.py
+++ backend/lib/latest.py
@@ -5,10 +5,18 @@ from flask import Request, Response
 from .db import engine
 from .net import HEADERS
 
-fetch_latest = text('''
+fetch_latest_1 = text('''
     SELECT id, image, votes, tournaments, descriptor, landmarks, gender_age,  geolocation
     FROM faces
-    ORDER BY random()
+    where last_shown_1 is null
+    ORDER BY id asc
+    LIMIT 1
+''')
+fetch_latest_2 = text('''
+    SELECT id, image, votes, tournaments, descriptor, landmarks, gender_age,  geolocation
+    FROM faces
+    where last_shown_1 is not null
+    ORDER BY last_shown_1 asc
     LIMIT 1
 ''')
 
@@ -18,14 +26,20 @@ def get_latest_handler(request: Request):
         return Response('', headers=HEADERS)
     if request.method == 'GET':
         with engine.connect() as connection:
-            rows = connection.execute(fetch_latest)
+            rows = connection.execute(fetch_latest_1)
             result = None
             for row in rows:
                 row = dict(row)
                 result = row
                 break
+            if result is None:
+                rows = connection.execute(fetch_latest_2)
+                for row in rows:
+                    row = dict(row)
+                    result = row
+                    break
         response = dict(
-            success=True, record=result
+            success=result is not None, record=result
         )
         response = Response(
             json.dumps(response),
