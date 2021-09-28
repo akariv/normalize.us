b1f8b5568d44aaef9ba92e27ba86d803bd6e56f9
Adam Kariv <adam.kariv@gmail.com>
2021-09-28 22:26:18 +0300

    More info in latest request

--- backend/lib/latest.py
+++ backend/lib/latest.py
@@ -9,14 +9,26 @@ from .net import HEADERS
 UPDATE_KEY = os.environ.get('UPDATE_KEY')
 
 fetch_latest_1 = text('''
-    SELECT id, image, votes, tournaments, descriptor, landmarks, gender_age,  geolocation
+    SELECT id, image, votes, tournaments, 
+        votes_0, tournaments_0, 
+        votes_1, tournaments_1, 
+        votes_2, tournaments_2, 
+        votes_3, tournaments_3, 
+        votes_4, tournaments_4,
+        descriptor, landmarks, gender_age, geolocation, created_timestamp
     FROM faces
     where last_shown_1 is null
     ORDER BY id asc
     LIMIT 1
 ''')
 fetch_latest_2 = text('''
-    SELECT id, image, votes, tournaments, descriptor, landmarks, gender_age,  geolocation
+    SELECT id, image, votes, tournaments, 
+        votes_0, tournaments_0, 
+        votes_1, tournaments_1, 
+        votes_2, tournaments_2, 
+        votes_3, tournaments_3, 
+        votes_4, tournaments_4,
+        descriptor, landmarks, gender_age, geolocation, created_timestamp
     FROM faces
     where last_shown_1 is not null
     ORDER BY last_shown_1 asc
