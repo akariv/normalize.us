ae6db6cd5fecff247732689bc546a5706eb6ba9e
Adam Kariv <adam.kariv@gmail.com>
2021-09-30 16:57:39 +0300

    More stretch for size

--- projects/normalize/src/app/face-processor.service.ts
+++ projects/normalize/src/app/face-processor.service.ts
@@ -62,7 +62,7 @@ export class FaceProcessorService {
   public detectorOptions: any;
   public defaultSnap = {
     orientaton: 10,
-    size: 50,
+    size: 75,
     distance: 0.1
   };
   public defaultScale = 1;
