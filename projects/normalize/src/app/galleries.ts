cbc2daa69b323bf517b5b59016f095f6686f59d3
Mushon Zer-Aviv <mushon@shual.com>
2022-03-28 12:26:22 +0300

    Update galleries.ts

--- projects/normalize/src/app/galleries.ts
+++ projects/normalize/src/app/galleries.ts
@@ -1,5 +1,5 @@
 export const Galleries = {
     dublin: [53.3441249, -6.2524838],
     moscow: [55.7546161, 37.6363133],
-    dortmund: [51.514750, 7.453194]
+    dortmund: [51.515070, 7.453080]
 };
